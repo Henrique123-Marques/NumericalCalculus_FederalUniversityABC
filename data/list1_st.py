caaca49d33e6d8d2a474ca8f1c3c51a2ceeed680
Henrique Marques Santos Silva <henriquemarquessantossilva@hotmail.com>
2025-02-25 14:58:11 -0300

    commit_25_2_2025_6

--- data/list1_st.py
+++ data/list1_st.py
@@ -92,9 +92,9 @@ pages = {
 }
 
 page = st.sidebar.selectbox("Escolha um exercício", list(pages.keys()))
-	st.write("""Site desenvolvido por - Henrique Marques Santos Silva, RA: 11202231350""")
-#Link dos codigos
 
+#Link dos codigos
+st.write("""Site desenvolvido por - Henrique Marques Santos Silva, RA: 11202231350""")
 st.write("""Link dos códigos: C#, Google Collab etc: https://drive.google.com/drive/folders/1DClCvIA3w7xjVwtnE0LNHbbhwp_aVsjl?usp=drive_link""")
 
 # Exercício 1 - IEEE 754
